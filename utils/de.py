4770f0f409a4518b67b601bf7a97fee83f0a8ede
Manan Soni <manansoni1399@gmail.com>
2020-11-04 19:52:05 +0530

    implement predictor-corrector methods

--- utils/de.py
+++ utils/de.py
@@ -161,6 +161,78 @@ class DE(object):
                 y = y + (h/24) * (55*self.f(x,y) - 59*self.f(xp,yp) + 37*self.f(xpp,ypp) - 9*self.f(xppp, yppp))
             x = x + h
 
+    def adam_pred_correct(self, h, interval):
+        """
+        Adom bashforth method (predictor - corrcector)
+        Uses euler's method for initial points
+        """
+        self.ans = []
+        a,b = interval
+        x = self.x0
+        y = self.y0
+        order = 4
+        while x <= b:
+            self.ans.append({'x': x, 'y': y})
+            if len(self.ans) < order:
+                y = y + h * self.f(x,y)
+            else:
+                xp,yp     = self.ans[-2]['x'], self.ans[-2]['y']
+                xpp,ypp   = self.ans[-3]['x'], self.ans[-3]['y']
+                xppp,yppp = self.ans[-4]['x'], self.ans[-4]['y']
+                y0 = y + (h/24) * (55*self.f(x,y) - 59*self.f(xp,yp) + 37*self.f(xpp,ypp) - 9*self.f(xppp, yppp))
+                y = y + (h/24) * (9*self.f(x+h,y0) + 19*self.f(x,y) - 5*self.f(xp,yp) + self.f(xpp, ypp))
+            x = x + h
+
+    def milne_pred_correct(self, h, interval):
+        """
+        Milne's method (predictor - corrcector)
+        Uses euler's method for initial points
+        """
+        self.ans = []
+        a,b = interval
+        x = self.x0
+        y = self.y0
+        order = 4
+        while x <= b:
+            self.ans.append({'x': x, 'y': y})
+            if len(self.ans) < order:
+                y = y + h * self.f(x,y)
+            else:
+                xp,yp     = self.ans[-2]['x'], self.ans[-2]['y']
+                xpp,ypp   = self.ans[-3]['x'], self.ans[-3]['y']
+                xppp,yppp = self.ans[-4]['x'], self.ans[-4]['y']
+                y0 = yppp + (4*h/3) * (2*self.f(xpp,ypp) - self.f(xp,yp) + 2*self.f(x,y))
+                y = yp + (h/3) * (self.f(xp,yp) + 4*self.f(x,y) + self.f(x+h,y0))
+            x = x + h
+
+    def adam_milne_pred_correct(self, h, interval, tol):
+        """
+        Adom milne method (predictor - corrcector)
+        Uses euler's method for initial points
+        Uses predictor formula till relative error < tol (or count > 50)
+        """
+        self.ans = []
+        a,b = interval
+        x = self.x0
+        y = self.y0
+        order = 4
+        while x <= b:
+            self.ans.append({'x': x, 'y': y})
+            if len(self.ans) < order:
+                y = y + h * self.f(x,y)
+            else:
+                xp,yp     = self.ans[-2]['x'], self.ans[-2]['y']
+                xpp,ypp   = self.ans[-3]['x'], self.ans[-3]['y']
+                xppp,yppp = self.ans[-4]['x'], self.ans[-4]['y']
+                y0 = y + (h/24) * (55*self.f(x,y) - 59*self.f(xp,yp) + 37*self.f(xpp,ypp) - 9*self.f(xppp, yppp))
+                y = y + (h/24) * (9*self.f(x+h,y0) + 19*self.f(x,y) - 5*self.f(xp,yp) + self.f(xpp, ypp))
+                count = 1
+                while abs((y-y0)/y) > tol and count < 50:
+                    count += 1
+                    y0 = y
+                    y = y0 + (h/24) * (9*self.f(x+h,y0) + 19*self.f(x,y) - 5*self.f(xp,yp) + self.f(xpp, ypp))
+            x = x + h
+
     def solve(self, method, xn, h=None, tol=None):
         method = method.lower()
         interval = (0,xn)
@@ -168,9 +240,9 @@ class DE(object):
         if (xn <= 0):
             raise Exception('Right endpoint can\'t be negative')
 
-        if method in ['euler', 'modified-euler', 'runge-kutta-4'] and not h:
+        if method in ['euler', 'modified-euler', 'runge-kutta-4', 'adam-milne-pc'] and not h:
             raise Exception(f'Method `{method}` requires step size `h` to be specified')
-        if method in ['adaptive-euler'] and not tol:
+        if method in ['adaptive-euler', 'adam-milne-pc'] and not tol:
             raise Exception(f'Method `{method}` requires a tolerance `tol` to be specified')
 
         if method == 'euler':
@@ -181,24 +253,33 @@ class DE(object):
             self.adaptive_euler(interval, tol)
         elif method == 'runge-kutta-4':
             self.runge_kutta(h, interval)
-        elif method == 'adom-bashforth-2':
+        elif method == 'adam-bashforth-2':
             self.adam_bashforth_2(h, interval)
-        elif method == 'adom-bashforth-3':
+        elif method == 'adam-bashforth-3':
             self.adam_bashforth_3(h, interval)
-        elif method == 'adom-bashforth-4':
+        elif method == 'adam-bashforth-4':
             self.adam_bashforth_4(h, interval)
+        elif method == 'milne-pc':
+            self.milne_pred_correct(h, interval)
+        elif method == 'adam-bashforth-pc':
+            self.adam_pred_correct(h, interval)
+        elif method == 'adam-milne-pc':
+            self.adam_milne_pred_correct(h, interval, tol)
         else:
             raise Exception(f'Method `{method}` not implemented')
 
 
-    def visualize(self):
+    def visualize(self, exact_sol=None):
         x = [val['x'] for val in self.ans]
         y = [val['y'] for val in self.ans]
 
         x,y = (list(t) for t in zip(*sorted(zip(x,y))))
         plt.plot(x, y, c='b', zorder=2, label='approximate solution')
-        plt.plot(x, np.exp(x), c='y', zorder=2, label='exact solution')
         plt.scatter(x, y, c='r', zorder=3)
+        if exact_sol:
+            ys = [exact_sol(x_) for x_ in x]
+            plt.plot(x, ys, c='y', zorder=2, label='exact solution')
+            plt.scatter(x, ys, c='k', zorder=3)
         plt.scatter(self.x0, self.y0, c='k', zorder=3)
         plt.legend(loc="upper left")
         plt.show()
